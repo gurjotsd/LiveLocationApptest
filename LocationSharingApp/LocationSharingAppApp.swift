a7dd8a0ec49c9a79a412826c461243711f524d08
gurjotsd <gurjotsinghdhillon14@gmail.com>
2025-02-15 18:00:17 +0000

    Initial Commit

---
+++ LocationSharingApp/LocationSharingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationSharingAppApp.swift
+//  LocationSharingApp
+//
+//  Created by Gurjot Dhillon on 15/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct LocationSharingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
